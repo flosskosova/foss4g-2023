6d4f492f5413c44930837ce80499d27164fa5e7b
Maria Arias de Reyna <ariasdereyna@redhat.com>
2020-06-14 19:20:38 +0200

    Upgrading jQuery to fix mobile menu

--- themes/foss4g2021/webpack.prod.js
+++ themes/foss4g2021/webpack.prod.js
@@ -2,7 +2,6 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
