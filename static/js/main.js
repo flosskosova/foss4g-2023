d7c40c6b06f2d0b76c18145a68ec1febd9db559c
Maria Arias de Reyna <ariasdereyna@redhat.com>
2021-03-08 22:05:54 +0100

    Using a vanilla JS version of the countdown instead of loading a bunch of useless JS

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,22 @@
-$(document).ready(function() {
-  $year = $("#countdown_dashboard").data("year");
-  $month = $("#countdown_dashboard").data("month");
-  $day = $("#countdown_dashboard").data("day");
+const deadline = new Date(2021, 8, 27, 9, 00);
 
-  $("#countdown_dashboard").countDown({
-    targetDate: {
-      day: $day,
-      month: $month,
-      year: $year,
-      hour: 8,
-      min: 59,
-      sec: 59
-    },
-    omitWeeks: true
-  });
-});
+
+setInterval(() => {
+  var currentTime = new Date();
+  var distance = deadline - currentTime;
+  
+  var days = Math.floor((distance / (1000 * 60 * 60 * 24)));
+  var hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+  var minutes = Math.floor((distance / (1000 * 60)) % 60);
+  var seconds = Math.floor((distance / 1000) % 60);
+  
+  
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+  
+  document.getElementById("countdowndays").innerHTML = days;
+  document.getElementById("countdownhours").innerHTML = hours;
+  document.getElementById("countdownminutes").innerHTML = minutes;
+  document.getElementById("countdownseconds").innerHTML = seconds;
+}, 500)
