7791eeb2b360e1d9bd3e9e51bf9d6ef0c76886cc
Jonathan Beliën <dev@jbelien.be>
2022-09-10 10:00:46 +0200

    🔧 Update rollup.config.js

--- themes/foss4g2021/rollup.config.js
+++ themes/foss4g2021/rollup.config.js
@@ -1,25 +1,20 @@
 // rollup.config.js
-import scss from 'rollup-plugin-scss';
-import resolve from '@rollup/plugin-node-resolve';
+import scss from "rollup-plugin-scss";
+import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: 'src/main.js',
+  input: "src/main.js",
   output: {
-    file: '../../static/js/main.js',
-    format: 'iife',
-// 		sourcemap: true
+    file: "../../static/js/main.js",
+    format: "iife",
   },
   plugins: [
     scss({
-// 		output: true,
-		output: '../../static/dist/main.min.css',
-    outputStyle: "compressed",
-	}),
-	resolve(),
-	terser(),
-  ]
-}
-
-
-
+      output: "../../static/dist/main.min.css",
+      outputStyle: "compressed",
+    }),
+    resolve(),
+    terser(),
+  ],
+};
